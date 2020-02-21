9500f1b61fa3b6de2975113766ea30b8e03cffe6
Renato Moitinho <renato.dias@b2wdigital.com>
2020-02-20 22:15:29 -0300

    opencv with rust

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# opencv-test
+# Opencv Test
+
+Install [opencv](https://docs.opencv.org/trunk/d7/d9f/tutorial_linux_install.html)
+Install [cargo](https://www.rust-lang.org/tools/install)
+ 
+## Clone project
+ ```sh
+ git clone https://github.com/renatomoitinho/opencv-test.git
+ ```
+## Build and create lib
+```sh
+cd opencv-test
+RUSTFLAGS="-C target-feature=-crt-static $(pkg-config opencv --libs)" cargo install --jobs 2 --path .
+```
+## run from anywhere :)
+ ```sh
+$ opencv-test /home/renato/Pictures/image.jpeg 800
+time to read image from buffer alpha=false w=Ok(700) h=Ok(700) time=7.84477ms
+time to resize w=Ok(800) h=Ok(800) time=2.799397ms
+time to read buffer size=89492 time=3.843157ms
+total time 14.526851ms
+ ```
+## Actions
+1. Load Matriz
+2. Generate thumbnails
+3. Expand 
+4. Convert PNG transparent to white pixels
+5. compress in JPG
+
+# opencv rust
+[code]([https://github.com/renatomoitinho/opencv-test/blob/master/src/main.rs](https://github.com/renatomoitinho/opencv-test/blob/master/src/main.rs))
+
+|input bytes| width x height| input format | output format | resize  | output bytes | time
+|--|--|--|--|--|--|--
+| 5.7MB | 4000x6000 | PNG | JPG | 1080x1080| 87.3Kb | 675ms
+| 529Kb | 789x789 | PNG | JPG | 700x700| 75.9Kb|37.5ms
+| 3.3MB | 2000x1614 | PNG | JPG | 1080x1080 |285Kb| 175.4ms 
+| 190.6Kb | 500x500 | PNG | JPG | 400x400 |39.8Kb | 15.5ms
+
+# Libvips rust
+[code]([https://github.com/renatomoitinho/vips-test/blob/master/src/main.rs](https://github.com/renatomoitinho/vips-test/blob/master/src/main.rs))
+
+|input bytes| width x height| input format | output format | resize  | output bytes | time (ms)
+|--|--|--|--|--|--|--
+| 5.7MB | 4000x6000 | PNG | JPG | 1080x1080| 54.8Kb | 1.13s
+| 529Kb | 789x789 | PNG | JPG | 700x700| 50.6Kb| 88ms
+| 3.3MB | 2000x1614 | PNG | JPG | 1080x1080 |190.6Kb| 310ms 
+| 190.6Kb | 500x500 | PNG | JPG | 400x400 |27.8Kb | 43.6ms
+
+## So info
+
+**Ubuntu 18.04.4 LTS**
+**memory:** 15,4 GiB
+**processor:** Intel® Core™ i7-6500U CPU @ 2.50GHz × 4 
+ **Graphics:** Intel® HD Graphics 520 (Skylake GT2)
+ **Os type:** 64-bit
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 use std::{env, fs};
-use std::fs::{File};
-use std::path::{Path, PathBuf};
+use std::fs::File;
 use std::io::prelude::*;
-use opencv::{core, imgcodecs, imgproc};
-use opencv::types::{VectorOfint, VectorOfuchar, VectorOfMat};
-use opencv::prelude::Vector;
+use std::path::{Path, PathBuf};
 use std::time::Instant;
+
+use opencv::{core, imgcodecs, imgproc};
+use opencv::prelude::Vector;
+use opencv::types::{VectorOfint, VectorOfMat, VectorOfuchar};
 
 #[derive(Debug, Clone, Copy)]
 pub struct ImageResize {
     pub width: i32,
     pub height: i32,
     pub vertical_border: i32,
-    pub horizontal_border: i32
+    pub horizontal_border: i32,
 }
 
 const WHITE_COLOR: f64 = 255 as f64;
 
-fn get_target_size(img_ref: core::Size, width: i32 , height: i32 ) -> ImageResize {
-
-    let radio: f32 = min( width as f32/ img_ref.width as f32, height as f32/ img_ref.height as f32);
-    let mut new_width : i32 = (img_ref.width as f32 * radio) as i32;
-    let mut new_height : i32 = (img_ref.height as f32 * radio) as i32;
+fn get_target_size(img_ref: core::Size, width: i32, height: i32) -> ImageResize {
+    let radio: f32 = min(width as f32 / img_ref.width as f32, height as f32 / img_ref.height as f32);
+    let mut new_width: i32 = (img_ref.width as f32 * radio) as i32;
+    let mut new_height: i32 = (img_ref.height as f32 * radio) as i32;
     let mut v_border = 0;
     let mut h_border = 0;
 
@@ -31,7 +31,7 @@ fn get_target_size(img_ref: core::Size, width: i32 , height: i32 ) -> ImageResiz
         new_width += (border as i32 % 1) * 2;
     } else if new_width > new_height {
         let border: f32 = (new_width - new_height) as f32 / 2.0;
-        v_border = {border as i32};
+        v_border = { border as i32 };
         new_height += ((border as i32) % 1) * 2;
     } else {
         v_border = 0;
@@ -42,12 +42,11 @@ fn get_target_size(img_ref: core::Size, width: i32 , height: i32 ) -> ImageResiz
         width: new_width,
         height: new_height,
         vertical_border: v_border,
-        horizontal_border: h_border
+        horizontal_border: h_border,
     }
 }
 
 fn min(n1: f32, n2: f32) -> f32 {
-
     if n1 < n2 {
         n1
     } else if n2 < n1 {
@@ -82,46 +81,25 @@ fn image_resize(image: &core::Mat, size: core::Size) -> Result<core::Mat, opencv
         size,
         0f64,
         0f64,
-        imgproc::INTER_LINEAR,
+        imgproc::INTER_AREA,
     ).expect("");
     Ok(result)
 }
 
 fn get_jpeg_buffer(image: &core::Mat) -> VectorOfuchar {
-
     let mut dest = VectorOfuchar::new();
     let mut quality = VectorOfint::with_capacity(2);
-    quality.push(99);
+    quality.push(90);
     quality.push(imgcodecs::IMWRITE_JPEG_QUALITY);
 
     imgcodecs::imencode(
         ".jpg",
         &image,
         &mut dest,
-        &quality
+        &quality,
     ).expect("");
 
     dest
-}
-
-fn remove_alpha(image: &core::Mat) -> Result<core::Mat, opencv::Error> {
-
-    let empty_mat = core::Mat::default()?;
-    let mut split = VectorOfMat::new();
-
-    core::split(&image, &mut split)?;
-    let alpha = split.get(3)?;
-    let mut color = core::Mat::default()?;
-    split.remove(3)?;
-    core::merge(&split, &mut color)?;
-    let mut mask = core::Mat::default()?;
-    imgproc::threshold(&alpha, &mut mask, 254.0, 255.0, imgproc::THRESH_BINARY)?;
-    let mut not = core::Mat::default()?;
-    let mut result = core::Mat::default()?;
-    core::bitwise_not(&color, &mut not, &mask)?;
-    core::bitwise_not(&not, &mut result, &empty_mat)?;
-
-    Ok(result)
 }
 
 pub fn expand(src: &core::Mat, resize: ImageResize) -> Result<core::Mat, opencv::Error> {
@@ -129,10 +107,55 @@ pub fn expand(src: &core::Mat, resize: ImageResize) -> Result<core::Mat, opencv:
 
     core::copy_make_border(src, &mut result, resize.vertical_border, resize.vertical_border,
                            resize.horizontal_border, resize.horizontal_border, core::BORDER_CONSTANT,
-                           core::Scalar::new(WHITE_COLOR, WHITE_COLOR, WHITE_COLOR, WHITE_COLOR))
+                           core::Scalar::all(WHITE_COLOR))
         .expect("not load buffer");
     Ok(result)
 }
+
+fn is_alpha_channel(src: &core::Mat) -> Result<bool, opencv::Error> {
+    let mut split = VectorOfMat::new();
+    core::split(&src, &mut split)?;
+    Ok(split.len() == 4)
+}
+
+fn change_alpha_channels(src: &core::Mat) -> Result<core::Mat, opencv::Error> {
+    let mut split = VectorOfMat::new();
+
+    // split channels
+    core::split(&src, &mut split)?;
+
+    // set alpha
+    let mut alpha = VectorOfMat::with_capacity(1);
+    alpha.push(split.get(3)?);
+
+    // remove alpha
+    split.remove(3)?;
+
+    let mut image = core::Mat::default()?;
+    let mut alpha_image = core::Mat::default()?;
+
+    // merge
+    core::merge(&split, &mut image)?;
+    core::merge(&alpha, &mut alpha_image)?;
+
+    let mut bit_not = core::Mat::default()?;
+    let mut bit_not_dest = core::Mat::default()?;
+    let empty_mat = core::Mat::default()?;
+
+    // invert colors
+    core::bitwise_not(&alpha_image, &mut bit_not, &empty_mat)?;
+    imgproc::cvt_color(&bit_not, &mut bit_not_dest, imgproc::COLOR_GRAY2RGB, 0)?;
+
+    // bit and add
+    let mut bit_and = core::Mat::default()?;
+    let mut result = core::Mat::default()?;
+
+    core::bitwise_and(&image, &image, &mut bit_and, &alpha_image)?;
+    core::add(&bit_and, &bit_not_dest, &mut result, &empty_mat, 0)?;
+
+    Ok(result)
+}
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -147,37 +170,49 @@ fn main() {
 
     // Load image
     let mut image = read_image(&buffer[..]).unwrap();
-    println!("time to read image from buffer w={:?} h={:?} time={:?}", image.cols(), image.rows(), start.elapsed());
+
+    let is_alpha = is_alpha_channel(&image).unwrap();
+
+    println!("time to read image from buffer alpha={:?} w={:?} h={:?} time={:?}", is_alpha, image.cols(), image.rows(), start.elapsed());
 
     // Resize
     start = Instant::now();
     let positions = get_target_size(
-        core::Size{
+        core::Size {
             width: image.cols().unwrap(),
-            height: image.rows().unwrap()
+            height: image.rows().unwrap(),
         },
         square,
-        square
+        square,
     );
-    image = image_resize(&image, core::Size{ width: positions.width, height: positions.height }).unwrap();
+    image = image_resize(&image, core::Size { width: positions.width, height: positions.height }).unwrap();
     println!("time to resize w={:?} h={:?} time={:?}", image.cols(), image.rows(), start.elapsed());
 
     // Extend
-    start = Instant::now();
-    image = remove_alpha(&image).unwrap();
-    image = expand(&image, positions).unwrap();
-    println!("time to extend time  w={:?} h={:?} time={:?}", image.cols(), image.rows(), start.elapsed());
+    if image.cols().unwrap() != image.rows().unwrap() {
+        start = Instant::now();
+        image = expand(&image, positions).unwrap();
+        println!("time to extend time  w={:?} h={:?} time={:?}", image.cols(), image.rows(), start.elapsed());
+    }
+
+    if is_alpha {
+        start = Instant::now();
+        image = change_alpha_channels(&image).unwrap();
+        println!("time to remove alpha time  w={:?} h={:?} time={:?}", image.cols(), image.rows(), start.elapsed());
+    }
 
     // Read Buffer
     start = Instant::now();
     let buffer = get_jpeg_buffer(&image);
     println!("time to read buffer size={:?} time={:?}", buffer.len(), start.elapsed());
-
-    println!("total time {:?}", start_total.elapsed() );
+    println!("total time {:?}", start_total.elapsed());
 
     // Only write
     start = Instant::now();
-    write_file_in_disk(&buffer, path.parent().unwrap().join("saida.jpg"));
+    write_file_in_disk(&buffer, path.parent().unwrap().join(format!("out_{}_opencv.jpeg", path
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap())));
     println!("time to write disk size={:?} time={:?}", buffer.len(), start.elapsed());
-
 }
